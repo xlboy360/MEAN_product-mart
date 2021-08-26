d9a040ca95fb287af6850cbf3e700e31b7b35b3f
Juan Salgado <juan_jsc360@hotmail.com>
2021-08-26 15:17:48 -0500

    showed products component (lazyload)

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
