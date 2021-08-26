25c463c25fa36373c1ea3347a209f7df00445707
Juan Salgado <juan_jsc360@hotmail.com>
2021-08-26 15:56:06 -0500

    Added card component to products

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -5,10 +5,16 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from '../../components/products/products.component';
 import { ProductDataService } from 'src/app/services/product-data.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from 'src/app/material-module';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    HttpClientModule,
+    MaterialModule,
+  ],
   providers: [ProductDataService],
 })
 export class ProductsModule {}
