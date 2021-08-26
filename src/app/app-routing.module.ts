965a98003021b3636e72aaed25a6101b489540b1
Juan Salgado <juan_jsc360@hotmail.com>
2021-08-26 16:18:11 -0500

    Created navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { 
-    path:"",
-    pathMatch:"full",
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
     component: HomeComponent,
   },
   {
     path: 'products',
     pathMatch: 'full',
-    loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule),
+    loadChildren: () =>
+      import('./modules/products/products.module').then(
+        (m) => m.ProductsModule
+      ),
   },
 ];
 
