d0a354fb3db4be3f8bda007ea3662cc2aabeacc0
xlboy360 <48699121+xlboy360@users.noreply.github.com>
2021-09-01 13:48:37 -0500

    Creation of logIn / Register module

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, Subject } from 'rxjs';
+
+export interface User {
+  email: string;
+  fullName: string;
+  password: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  // Creating user structure
+  private user$ = new Subject<User>();
+
   constructor() {}
 
   login(email: string, password: string) {
     return of({ email, password });
+  }
+
+  getUser() { 
+    return this.user$.asObservable();
+  }
+
+  register(user: any) {
+    this.user$.next(user);
+    return of(user);
   }
 }
