a48ad4287dc1dc19e1442b1803d48f7f8315ca3b
xlboy360 <48699121+xlboy360@users.noreply.github.com>
2021-11-13 09:12:49 -0600

    Local changes in register files

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -9,20 +9,52 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  userGroup = new FormGroup({
+  userForm = new FormGroup({
     fullName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
+    repeatPassword: new FormControl('', [
+      Validators.required,
+      this.passwordsMatchValidator,
+    ]),
   });
 
   constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  passwordsMatchValidator(control: FormControl) {
+    const password = control.root.get('password');
+    return password && control.root.get('repeatPassword') !== password.value
+      ? {
+          passwordMatch: true,
+        }
+      : null;
+  }
 
   register() {
-    const user = this.userGroup.getRawValue();
+    if (this.userForm.valid) {
+      return;
+    }
+    const user = this.userForm.getRawValue();
     this.authService
       .register(user)
-      .subscribe((s) => this.router.navigate(['/login']));
+      .subscribe((s) => this.router.navigate(['/']));
   }
+
+  get fullname() {
+    return this.userForm.get('fullName');
+  }
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  get password() {
+    return this.userForm.get('password');
+  }
+
+  get repeatPassword() {
+    return this.userForm.get('repeatPassword');
+  }
+  
+  ngOnInit(): void {}
 }
